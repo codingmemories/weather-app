aea2fb342113a32d6cf70fe51eb194d8709065aa
codingmemories <you@example.com>
2022-10-07 22:35:59 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
+import React from "react";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/vasylyna-lykhogodenko/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vasylyna Lykhogodenko
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/codingmemories/weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/vasylyna-lykhogodenko/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vasylyna Lykhogodenko
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/codingmemories/weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
