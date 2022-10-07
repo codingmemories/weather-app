0426f6ccd56f68db32f661b575846d901648c7c8
codingmemories <you@example.com>
2022-10-07 22:22:55 +0300

    add starting project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/vasylyna-lykhogodenko/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Vasylyna Lykhogodenko
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/codingmemories/weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
