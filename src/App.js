f0f6c9068e621e356134f0cf630562a9996ea466
codingmemories <you@example.com>
2022-10-08 22:09:36 +0300

    add axios

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
         <footer>
           This project was coded by{" "}
           <a
